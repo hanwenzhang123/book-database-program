66dda876d976a478972bb4e8619696b54961a04b
hanwenzhang123 <hanwenzhang888@gmail.com>
2021-04-20 20:48:00 -0400

    add book to db function

--- app.py
+++ app.py
@@ -1,75 +1,135 @@
-from models import (Base, session,
-                    Book, engine)
-import datetime
+from models import Base, session, Book, engine
 import csv
+import time
 
 
 def menu():
-    while True:         # while loop - only end when the choice is returned and return will always cancel and stop a loop
+    '''
+    Main menu for the application
+    Returns menu choice from user
+    '''
+    while True:  # while loop - only end when the choice is returned and return will always cancel and stop a loop
         print('''
-                \nPROGRAMMING BOOKS
-                \r1) Add book
-                \r2) View all books
-                \r3) Search for book
-                \r4) Book Analysis
-                \r5) Exit''')
-        choice = input('What would you like to do?')
+          \nPROGRAMMING BOOKS
+          \r1) Add book
+          \r2) View all books
+          \r3) Search for a book
+          \r4) Book Analysis
+          \r5) Exit
+          ''')
+        choice = input('What would you like to do? ')
         if choice in ['1', '2', '3', '4', '5']:
             return choice
         else:
-            input('''
-            \rPlease choose one of the options above.
-            \rA number from 1-5.
-            \rPress enter to try again.''')
+            input('Please choose one of the menu items above. '
+                  'A number between 1-5.\nPress enter.')
 
 
-def clean_date(date_str):
+# add function
+# edit function
+# delete function
+# search function
+
+
+def clean_date(date):
+    '''
+    Cleans the date string
+    '''
     months = ['January', 'February', 'March', 'April', 'May', 'June',
               'July', 'August', 'September', 'October', 'November', 'December']
-    split_date = date_str.split(' ')
-    month = int(months.index(split_date[0]) + 1)
-    day = int(split_date[1].split(',')[0])
-    year = int(split_date[2])
-    return datetime.date(year, month, day)
+    split = date.split(' ')
+    try:
+        month = int(months.index(split[0]) + 1)
+        day = int(split[1].split(',')[0])
+        year = int(split[2])
+        return_date = datetime.date(year, month, day)
+    except ValueError:
+        input('''\n******** Date Error ********
+              \rThe date format should include a valid Month Day, Year from the past.
+              \rEx: January 13, 2003.
+              \rPress enter to try again.
+              \r*******************************''')
+        return
+    else:
+        return return_date
 
 
-def clean_price(price_str):
-    price_float = float(price_str)
-    return int(price_float * 100)
+def clean_price(price):
+    '''
+    Cleans the price string
+    '''
+    try:
+        price_float = float(price)
+    except ValueError:
+        input('''\n ******** Price Error ********
+              \rThe price should be a number without a currency symbol.
+              \rExample: 10.99
+              \rPress enter to try again.
+              \r***********************************
+              ''')
+        return
+    else:
+        price_cents = price_float * 100
+        return int(price_cents)
 
 
 def add_csv():
+    '''
+    Add books from csv to database 
+    if they aren't in the DB already
+    '''
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
         for row in data:
+            date = clean_date(row[2])
+            price = clean_price(row[3])
+            # either return the book or return none
             book_in_db = session.query(Book).filter(
                 Book.title == row[0]).one_or_none()
-            if book_in_db == None:
-                title = row[0]
-                author = row[1]
-                date = clean_date(row[2])
-                price = clean_price(row[3])
-                new_book = Book(title=title, author=author,
-                                published_date=date, price=price)
+            if book_in_db == None:  # it only gets created if it is not in the database
+                new_book = Book(
+                    title=row[0], author=row[1], published_date=date, price=price)
                 session.add(new_book)
-        session.commit()
+    session.commit()
 
 
 def app():
+    '''
+    Main application function
+    '''
     app_running = True
     while app_running:
         choice = menu()
         if choice == '1':
             # add book
-            pass
+            title = input('Title: ')
+            author = input('Author: ')
+            date_error = True
+            while date_error:
+                date = input('Published Date (Ex: October 25, 2017): ')
+                date = clean_date(date)
+                if type(date) == datetime.date:
+                    date_error = False
+            price_error = True
+            while price_error:
+                price = input('Price (Ex: 25.64): ')
+                price = clean_price(price)
+                if type(price) == int:
+                    price_error = False
+            new_book = Book(title=title, author=author,
+                            published_date=date, price=price)
+            session.add(new_book)
+            session.commit()
+            print('Book added!')
+            time.sleep(1.5)
         elif choice == '2':
             # view books
-            pass
+            for book in session.query(Book):
+                print(f'{book.id} | {book.title} | {book.author}')
+            input('\nPress enter to return to the main menu.')
         elif choice == '3':
-            # search book
             pass
         elif choice == '4':
-            # analysis
             pass
         else:
             print('GOODBYE')
@@ -78,8 +138,5 @@ def app():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    # app()
     add_csv()
-
-    for book in session.query(Book):
-        print(book)
+    app()
--- models.py
+++ models.py
@@ -1,11 +1,12 @@
-from sqlalchemy import (create_engine, Column,
-                        Integer, String, Date)
+from sqlalchemy import create_engine, Column, String, Integer, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 engine = create_engine('sqlite:///books.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
+
 Base = declarative_base()
 
 
@@ -19,4 +20,4 @@ class Book(Base):
     price = Column('Price', Integer)
 
     def __repr__(self):
-        return f'Title: {self.title} Author: {self.author} Published: {self.published_date} Price: {self.price}'
+        return f'<Title: {self.title} Author: {self.author} Published: {self.published_date} Price: {self.price}>'
